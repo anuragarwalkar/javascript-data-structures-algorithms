d9cdb34d4f5477ceb89982f64d40ee6e8f53cefb
Anurag <aarwalka@cisco.com>
2020-10-22 22:05:05 +0530

    updated questions

--- src/excercise/inteviewQuestions.js
+++ src/excercise/inteviewQuestions.js
@@ -2,13 +2,15 @@
 const items = ['asas', null, "", undefined];
 
 // return most frequest item
-const mostFrequentItem = [1, 1, 2, 2, 3, 4, 'a', 5, 'a', 'u', 'a', 'a'];
+const mostFrequentItem = [1, 'a', 2, 2, 3, 4, 'a', 5, 'a', 'u', 'a', 'a'];
 
 // Remove duplicates from array
 const nums = [1, 2, 2, 3];
 
 // Flattern 2D Array
 const twoDimentionalArray = [[1, 2, 3], [4, 5]];
+
+// console.log(twoDimentionalArray.reduce((a, b) => a.concat(b), []))
 
 // Console log
 console.log(Promise.resolve('1'))
@@ -25,9 +27,9 @@ function y() {
 }
 const x = {
     value: 1000,
-    items: 1,
+    length: 1,
     getItems: function() {
-        arguments[0]();
+        arguments[0]()
     }
 }
 
@@ -36,7 +38,7 @@ x.getItems(y, 2);
 // Tell me the output
 console.log(5 < 6 < 7);
 
-console.log(5 > 6 > 7);
+console.log(7 > 6 > 5);
 
 // Tell me the output
 console.log(2 + '2');
