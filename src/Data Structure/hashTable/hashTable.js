85d4a783b2c5e4e6a79ad41dc5567505699996b5
Anurag Arwalkar <anuragarwalkar@gmail.com>
2021-03-15 12:25:29 +0530

    formating

--- src/Data Structure/hashTable/hashTable.js
+++ src/Data Structure/hashTable/hashTable.js
@@ -1,76 +1,77 @@
 class HashTable {
-    constructor(size = 53) {
-        this.keyMap = Array.from({length:53},()=> ([]));
+  constructor(size = 53) {
+    this.keyMap = Array.from({ length: 53 }, () => []);
+  }
+
+  _hash(key) {
+    let total = 0;
+    const wiredPrime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const value = key[i];
+      let valResult = value.charCodeAt(0) - 96;
+      total = (total + wiredPrime + valResult) % this.keyMap.length;
     }
 
-    _hash(key) {
-        let total = 0;
-        const wiredPrime = 31;
-        for(let i = 0; i < Math.min(key.length, 100); i++){
-            const value = key[i];
-            let result = value.charCodeAt(0) - 96;
-            total = (total + wiredPrime + result) % this.keyMap.length;
-        }
-    
-        return total;
-    }
+    return total;
+  }
 
-    set(key, value){
-        const hashed = this._hash(key);
-        
-        this.keyMap[hashed].push([key, value]);
-    }
-
-    get(key) {
+  set(key, value) {
     const hashed = this._hash(key);
-    if(!hashed) return null;
+
+    this.keyMap[hashed].push([key, value]);
+  }
+
+  get(key) {
+    const hashed = this._hash(key);
+    if (!hashed) return null;
     for (const item of this.keyMap[hashed]) {
-        const [itemKey] = item;
-        if(itemKey === key) return item;
+      const [itemKey] = item;
+      if (itemKey === key) return item;
     }
-    return null
-    }
+    return null;
+  }
 
-    values () {
-        const valuesArray = [];
+  values() {
+    const valuesArray = [];
 
-        for (const iterator of this.keyMap) {
-            if(iterator.length === 0 || !iterator) continue;
+    for (const iterator of this.keyMap) {
+      if (iterator.length === 0 || !iterator) continue;
 
-            for(const item of iterator){
-                const [_, lastIndex] = item;
-                if(!valuesArray.includes(lastIndex))
-                valuesArray.push(lastIndex);
-            }    
+      for (const item of iterator) {
+        const [_, lastIndex] = item;
+        if (!valuesArray.includes(lastIndex)) {
+          valuesArray.push(lastIndex);
         }
-
-        return valuesArray;
+      }
     }
 
-    keys() {
-        const valuesArray = [];
+    return valuesArray;
+  }
 
-        for (const iterator of this.keyMap) {
-            if(iterator.length === 0 || !iterator) continue;
+  keys() {
+    const valuesArray = [];
 
-            for(const item of iterator){
-                const [firstIndex] = item;
-                if(!valuesArray.includes(firstIndex))
-                valuesArray.push(firstIndex);
-            }    
+    for (const iterator of this.keyMap) {
+      if (iterator.length === 0 || !iterator) continue;
+
+      for (const item of iterator) {
+        const [firstIndex] = item;
+        if (!valuesArray.includes(firstIndex)) {
+          valuesArray.push(firstIndex);
         }
-
-        return valuesArray; 
+      }
     }
+
+    return valuesArray;
+  }
 }
 
 const result = new HashTable();
 
-result.set('name', 'anurag');
-result.set('fullName', 'anurag');
+result.set("name", "anurag");
+result.set("fullName", "anurag");
 
-result.set('lastName', 'arwalkar');
+result.set("lastName", "arwalkar");
 
-console.log('keys:', result.values());
-console.log('values:', result.keys());
-
+console.log("keys:", result.values());
+console.log("values:", result.keys());
