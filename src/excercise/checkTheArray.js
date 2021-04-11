9e05cc4fbb8a649f8f4469542e2a06baf3f2b60b
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-04-11 17:07:43 +0530

    fixed logic

--- src/excercise/checkTheArray.js
+++ src/excercise/checkTheArray.js
@@ -1,12 +1,27 @@
 const same = (input, output) => {
-  const converted = input.reduce((a, b) => {
-    a[b] ? null : (a[b] = b);
-    return a;
-  }, {});
+  if (input.length !== output.length) {
+    return false;
+  }
+
+  const createFrequency = (array) =>
+    array.reduce((a, b) => {
+      a[b] ? ++a[b] : (a[b] = 1);
+      return a;
+    }, {});
+
+  const convertedInput = createFrequency(input);
 
   let isSame = output.every(function (e) {
     const sqrt = Math.sqrt(e);
-    return converted[sqrt] !== undefined;
+    const isValid = convertedInput[sqrt] !== undefined;
+    if (convertedInput[sqrt]) {
+      if (convertedInput[sqrt] > 1) {
+        convertedInput[sqrt] = --convertedInput[sqrt];
+      } else {
+        delete convertedInput[sqrt];
+      }
+    }
+    return isValid;
   });
 
   return isSame;
