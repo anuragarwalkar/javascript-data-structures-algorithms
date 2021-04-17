e07640417f097ac3701a2c337ac4ff442d3f1cb0
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-04-17 16:20:44 +0530

    added new method to count unique values

--- src/excercise/countUniqValues.js
+++ src/excercise/countUniqValues.js
@@ -20,9 +20,25 @@ const countUniqValues = (array) => {
   return count;
 };
 
+const countUniqValuesAnother = (array) => {
+  const clonedArray = [...array];
+
+  let i = 0;
+  for (let j = 1; j < clonedArray.length; j++) {
+    if (clonedArray[i] !== clonedArray[j]) {
+      i++;
+      clonedArray[i] = clonedArray[j];
+    }
+  }
+
+  return clonedArray.slice(0, i + 1);
+};
+
 // const result = countUniqValues([1, 1, 1, 1, 1, 2]); //Result: 2
 // const result = countUniqValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); //Result: 7
 // const result = countUniqValues([]); //Result: 0
-const result = countUniqValues([-2, -1, -1, 0, 1]); //Result: 4
+// const result = countUniqValues([-2, -1, -1, 0, 1]); //Result: 4
+
+const result = countUniqValuesAnother([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); //Result: 2
 
 console.log(result);
