3a929727bf6856acab59ecd32bc47cfed5948ee6
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-04-17 23:46:50 +0530

    fixed logic

--- src/excercise/newExcercise/isSubsequence.js
+++ src/excercise/newExcercise/isSubsequence.js
@@ -2,27 +2,37 @@ const isSubsequence = (string1, string2) => {
   let pointerOne = 0;
   let pointerTwo = 0;
 
-  let matchCount = 0;
-
-  for (let index = 0; index < string2.length; index++) {
+  while (pointerTwo < string2.length) {
     if (string1[pointerOne] === string2[pointerTwo]) {
-      matchCount++;
       pointerOne++;
-      pointerTwo++;
-      if (pointerOne >= string1.length) {
-        break;
-      }
-    } else {
-      if (pointerTwo < string2.length) {
-        pointerTwo++;
-      } else {
-        pointerOne++;
-      }
     }
+
+    if (pointerOne === string1.length) {
+      return true;
+    }
+
+    pointerTwo++;
   }
-  return string1.length === matchCount;
+  return false;
 };
 
 const result = isSubsequence("hello", "hello");
 
 console.log(result);
+
+//   for (let index = 0; index < string2.length; index++) {
+//     if (string1[pointerOne] === string2[pointerTwo]) {
+//       matchCount++;
+//       pointerOne++;
+//       pointerTwo++;
+//       if (pointerOne >= string1.length) {
+//         break;
+//       }
+//     } else {
+//       if (pointerTwo < string2.length) {
+//         pointerTwo++;
+//       } else {
+//         pointerOne++;
+//       }
+//     }
+//   }
