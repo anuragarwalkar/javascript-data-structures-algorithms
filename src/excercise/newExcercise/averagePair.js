a847d855b17af7f90ce401069710064501bdf48c
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-04-17 23:40:01 +0530

    updated the logic

--- src/excercise/newExcercise/averagePair.js
+++ src/excercise/newExcercise/averagePair.js
@@ -1,27 +1,40 @@
-const averagePair = (givenArray, expected) => {
-  let pointerA = 0;
-  let pointerB = 1;
+const averagePair = (givenArray, expectedAvg) => {
+  let start = 0;
+  let end = givenArray.length - 1;
+  while (start < end) {
+    const avg = givenArray[start] + givenArray[end] / 2;
 
-  while (pointerA < givenArray.length) {
-    const result = givenArray[pointerA] + givenArray[pointerB] / 2;
-
-    if (result === expected) {
+    if (avg === expectedAvg) {
       return true;
+    } else if (avg < expectedAvg) {
+      start++;
     } else {
-      if (pointerB + 1 === givenArray.length) {
-        pointerA++;
-        if (pointerA + 1 < givenArray.length) {
-          pointerB = pointerA + 1;
-        }
-      } else {
-        pointerB++;
-      }
+      end--;
     }
   }
-
   return false;
 };
 
 const result = averagePair([1, 2, 3], 2.5);
 
 console.log(result);
+
+// let pointerA = 0;
+// let pointerB = 1;
+
+//   while (pointerA < givenArray.length) {
+//     const result = givenArray[pointerA] + givenArray[pointerB] / 2;
+
+//     if (result === expected) {
+//       return true;
+//     } else {
+//       if (pointerB + 1 === givenArray.length) {
+//         pointerA++;
+//         if (pointerA + 1 < givenArray.length) {
+//           pointerB = pointerA + 1;
+//         }
+//       } else {
+//         pointerB++;
+//       }
+//     }
+//   }
