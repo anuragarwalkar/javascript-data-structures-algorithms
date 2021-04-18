f0391a623f476c9626f1b7cb24837bc940661ef1
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-04-18 21:04:13 +0530

    added longest substring

--- src/excercise/newExcercise/findLongestSubstring.js
+++ src/excercise/newExcercise/findLongestSubstring.js
@@ -4,13 +4,14 @@ const findLongestSubstring = (givenString) => {
   let seen = {};
 
   for (let i = 0; i < givenString.length; i++) {
-    let char = givenString[i];
+    const char = givenString[i];
+
     if (seen[char]) {
       start = Math.max(start, seen[char]);
     }
-    // index - beginning of substring + 1 (to include current in count)
+
     longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
+
     seen[char] = i + 1;
   }
 
@@ -18,6 +19,7 @@ const findLongestSubstring = (givenString) => {
 };
 
 // const result = findLongestSubstring("rithmschool");
-const result = findLongestSubstring("thisisawesome");
+// const result = findLongestSubstring("thisisawesome");
+const result = findLongestSubstring("longestsubstring");
 
-console.log(result);
+console.log("longest".length);
