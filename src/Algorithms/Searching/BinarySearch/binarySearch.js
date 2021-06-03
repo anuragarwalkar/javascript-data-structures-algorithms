98df114a6e66d752feaf698e268dd098cd5ab014
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-06-03 10:41:09 +0530

    binary serach ex

--- src/Algorithms/Searching/BinarySearch/binarySearch.js
+++ src/Algorithms/Searching/BinarySearch/binarySearch.js
@@ -94,3 +94,76 @@ console.log("result:", result);
 //   const result = binarySearch([1, 2, 3, 4, 5], 5);
 
 //   console.log(result);
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.right = null;
+//     this.left = null;
+//   }
+// }
+
+// class BinarySearchTree {
+//   constructor() {
+//     this.root = null;
+//   }
+
+//   find(val) {
+//     if (this.root == null) {
+//       return null;
+//     }
+
+//     let node = this.root;
+
+//     while (node != null && node.value !== val) {
+//       if (node.value < val) {
+//         node = node.right;
+//       } else if (node.value > val) {
+//         node = node.left;
+//       }
+//     }
+
+//     return node;
+//   }
+
+//   contains(val) {
+//     return !!this.find(val);
+//   }
+
+//   insert(val) {
+//     if (this.root == null) {
+//       return (this.root = new Node(val));
+//     }
+//     let node = this.root;
+//     while (node != null && node.value !== val) {
+//       if (node.value < val) {
+//         if (node.right != null) {
+//           node = node.right;
+//         } else {
+//           node.right = new Node(val);
+//           break;
+//         }
+//       }
+
+//       if (node.value > val) {
+//         if (node.left != null) {
+//           node = node.left;
+//         } else {
+//           node.left = new Node(val);
+//           break;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// const bst = new BinarySearchTree();
+
+// bst.insert(5);
+// bst.insert(4);
+// bst.insert(8);
+// bst.insert(10);
+// bst.insert(15);
+// bst.insert(20);
+
+// console.log(bst.contains(110));
