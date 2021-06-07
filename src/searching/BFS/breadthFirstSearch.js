a335367cb92221241e7468f0d924973b8a65bbe7
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-06-07 13:12:26 +0530

    added breadth first search

---
+++ src/searching/BFS/breadthFirstSearch.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  find(val) {
+    if (this.root == null) {
+      return null;
+    }
+
+    let node = this.root;
+
+    while (node != null && node.value !== val) {
+      if (node.value < val) {
+        node = node.right;
+      } else if (node.value > val) {
+        node = node.left;
+      }
+    }
+
+    return node;
+  }
+
+  contains(val) {
+    return !!this.find(val);
+  }
+
+  insert(val) {
+    if (this.root == null) {
+      return (this.root = new Node(val));
+    }
+    let node = this.root;
+    while (node != null && node.value !== val) {
+      if (node.value < val) {
+        if (node.right != null) {
+          node = node.right;
+        } else {
+          node.right = new Node(val);
+          break;
+        }
+      }
+
+      if (node.value > val) {
+        if (node.left != null) {
+          node = node.left;
+        } else {
+          node.left = new Node(val);
+          break;
+        }
+      }
+    }
+  }
+
+  BFS() {
+    let data = [];
+    let node = this.root;
+    let queue = [node];
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+}
+
+const bst = new BinarySearchTree();
+
+bst.insert(5);
+bst.insert(4);
+bst.insert(8);
+bst.insert(10);
+bst.insert(15);
+bst.insert(20);
+
+console.log(bst.BFS());
