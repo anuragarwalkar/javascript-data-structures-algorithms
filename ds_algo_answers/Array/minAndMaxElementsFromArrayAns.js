51cb92dd6fadfe77b36ad3afa4eadbd2bd79eac0
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-07-12 11:55:59 +0530

    update

--- ds_algo_answers/Array/minAndMaxElementsFromArrayAns.js
+++ ds_algo_answers/Array/minAndMaxElementsFromArrayAns.js
@@ -1,12 +1,7 @@
 const findMinAndMax = (array) => {
-  let min = Number.MIN_SAFE_INTEGER;
-  let max;
   for (const item of array) {
-    if (min < item) {
-      min = item;
-    }
+    console.log(item);
   }
-  return min;
 };
 
 console.log(findMinAndMax([10, 50, 12, 16, 2]));
