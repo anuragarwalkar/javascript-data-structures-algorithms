1d4e36641b90fc684cb8a071ce254fb9080b0203
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-07-15 10:34:27 +0530

    fixed

--- ds_algo_answers/Array/reverseTheArrayAns.js
+++ ds_algo_answers/Array/reverseTheArrayAns.js
@@ -1,21 +1,32 @@
-//   return array.reverse();
-//   const items = [];
-//   for (let index = array.length - 1; index >= 0; index--) {
-//     const element = array[index];
-//     items.push(element);
-//   }
-//   return items;
-//   return array.reduce((a, b) => {
-//     a.unshift(b);
-//     return a;
-//   }, []);
+// const reverseTheArray = (array) => {
+//   let middle = Math.floor(array.length / 2);
+//   let j = array.length - 1;
 
+//   for (let i = 0; i < middle; i++) {
+//     let val = array[j];
+//     array[j] = array[i];
+//     array[i] = val;
+//     j--;
+//   }
+
+//   console.log(array);
+// };
+
+// o n/2
 const reverseTheArray = (array) => {
-  if (array.length === 1) {
-    return array[0];
+  const newArray = [...array];
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    let val = newArray[end];
+    newArray[end] = newArray[start];
+    newArray[start] = val;
+    start++;
+    end--;
   }
-  const res = reverseTheArray(array.splice(0, array.length - 1));
-  return array.concat(res);
+
+  console.log(newArray);
 };
 
-console.log(reverseTheArray([1, 2, 3]));
+console.log(reverseTheArray([1, 2, 3, 4, 5, 6]));
