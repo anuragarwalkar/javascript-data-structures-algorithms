936c49b086ee13f64655113d23021a31804038f5
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-07-15 12:53:05 +0530

    wip

--- index.js
+++ index.js
@@ -17,12 +17,28 @@ const findMinAndMax = (array, low, high) => {
   if (low === high) {
     minMax.max = array[low];
     minMax.min = array[low];
+    return minMax;
   }
+
+  //   If there are two elements;
+  if (high === low + 1) {
+    if (array[low] < array[high]) {
+      minMax.min = array[low];
+      minMax.max = array[high];
+    } else {
+      minMax.min = array[high];
+      minMax.max = array[low];
+    }
+
+    return minMax;
+  }
+
+  // If there are more than 2 elements
 
   return minMax;
 };
 
 // const array = [10, 50, 12, 16, 2];
-const array = [10];
+const array = [10, 50];
 
 console.log(findMinAndMax(array, 0, array.length - 1));
