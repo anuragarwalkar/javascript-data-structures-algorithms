07e302cb74a332f1bf48a5eb3e59ce0b7b986282
Anurag Arwalkar <anruagarwalkar@gmail.com>
2021-07-17 15:27:53 +0530

    added reverse

--- src/DS Algo Questions/String/reverseAString.js
+++ src/DS Algo Questions/String/reverseAString.js
@@ -1,6 +1,18 @@
 const newString = ["a", "n", "u", "r", "a", "g"];
 
-const reverseAString = () => {};
+const reverseAString = (givenArray) => {
+  let start = 0;
+  let end = givenArray.length - 1;
+
+  while (start < end) {
+    const temp = givenArray[end];
+    givenArray[end] = givenArray[start];
+    givenArray[start] = temp;
+    start++;
+    end--;
+  }
+  return givenArray;
+};
 
 const result = reverseAString(newString);
 
