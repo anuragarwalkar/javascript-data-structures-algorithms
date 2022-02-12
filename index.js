29f40dc038804f8c39251565e4958e905c3d0177
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-02-12 19:08:07 +0530

    added optimual solution

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function twoSum(array, target) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === target) {
-        return [i, i + 1];
-      }
+function twoSum(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      return [map[nums[i]], i];
+    } else {
+      let ntf = target - nums[i];
+      map[ntf] = i;
     }
   }
+  return null;
 }
 
 const result = twoSum([1, 3, 7, 9, 2], 11);
--- src/FAANG/two-sum.js
+++ src/FAANG/two-sum.js
@@ -1,11 +1,25 @@
 function twoSum(array, target) {
   for (let i = 0; i < array.length; i++) {
+    let numberToFind = array[i] - target;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === target) {
+      if (numberToFind === target) {
         return [i, i + 1];
       }
     }
   }
+}
+
+function twoSum(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      return [map[nums[i]], i];
+    } else {
+      let ntf = target - nums[i];
+      map[ntf] = i;
+    }
+  }
+  return null;
 }
 
 const result = twoSum([1, 3, 7, 9, 2], 11);
