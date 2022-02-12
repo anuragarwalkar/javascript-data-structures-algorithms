cc758d036820fd14c8dd8b9e120d9c847fb0e8ef
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-02-12 17:29:11 +0530

    removed

--- src.js
+++
@@ -1,7 +0,0 @@
-function blockEventLoop(i) {
-  console.log(i);
-  blockEventLoop(i + 1);
-}
-
-const btn = document.getElementById("myButton");
-btn.addEventListener("click", () => blockEventLoop(0));
