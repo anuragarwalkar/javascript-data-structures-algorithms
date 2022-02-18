84e375302754c706ea45136d836b93e89b92b9f5
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-02-18 12:43:23 +0530

    adde

---
+++ src/FAANG/linked-list.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  head = null;
+  tail = null;
+
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      return node;
+    }
+
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  reverse() {
+    let current = this.head;
+    let next = null;
+    let prev = null;
+
+    while (current) {
+      next = current.next;
+
+      current.next = prev;
+
+      if (!prev) {
+        this.tail = current;
+      }
+
+      prev = current;
+      current = next;
+    }
+    this.head = prev;
+  }
+}
+
+const ls = new LinkedList();
+
+ls.add(1);
+ls.add(2);
+ls.add(3);
+ls.add(4);
+ls.add(5);
+ls.reverse();
+
+//      p c    n
+//  null  1 => 2 => 3 => 4 => 5
