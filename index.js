03f2803279fda1d23a302c0313aaa865fb44539a
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-11-26 12:22:57 +0530

    initial commit for graph

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 class Graph {
+    adjacencyList = {
 
+    }
+
+    addVertex(vertex) {
+        if (this.adjacencyList[vertex]) {
+            return
+        }
+        this.adjacencyList[vertex] = [];
+    }
 }
+
+const graph = new Graph();
+
+graph.addVertex("Tokeyo")
 
