cd25102123e1ea5c3547d0f869fb64a58eb59082
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-11-29 11:03:51 +0530

    adde

--- solution.js
+++ solution.js
@@ -1,19 +1,25 @@
 const result = [];
 
-const getSumBy = (i) => {
-    const splitResult = i.toString().split('');
+const getSumBy = (i, condition = false) => {
+    const splitResult = i.toString().split(''); // ['6', '0', '3', '0']
 
-    if (!splitResult.find(n => n === '8')) {
-        return splitResult.reduce((a, b) => a + parseInt(b), 0)
+    // if below line return false then do reset
+    if (!splitResult.find(n => n === '8') || condition) {
+        // parseInt is to convert string to number
+        return splitResult.reduce((a, b) => a + parseInt(b), 0);
     }
 
+    // 6038
     return -1;
 };
 
+// ex 6030
+// ex 6038
 for (let i = 6025; i <= 9999; i++) {
-    const first = getSumBy(i);
+    const first = getSumBy(i); // 9
+
     if (first !== -1) {
-        const second = getSumBy(first);
+        const second = getSumBy(first, true);
         if (second === 9) {
             result.push(i);
         }
