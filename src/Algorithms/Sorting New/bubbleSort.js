16cd1c071c5de513cd098c3c7405c2bd9aca02f1
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-12-02 11:56:58 +0530

    adde

---
+++ src/Algorithms/Sorting New/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const result = bubbleSort([2, 5, 8, 4, 1, 10]);
+
+console.log("result:", result);
